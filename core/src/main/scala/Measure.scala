827ec26b99a3bb61c79d2a5a8ff05ddeb14dceed
Grant Beaty <gbeaty@gmail.com>
2014-08-30 22:37:36 -0400

    Comparison operators.

--- core/src/main/scala/Measure.scala
+++ core/src/main/scala/Measure.scala
@@ -7,11 +7,16 @@ case class Measure[D <: Dims](v: Double) extends AnyVal {
   def +(m: Measure[D]) = Measure[D](v + m.v)
   def -(m: Measure[D]) = Measure[D](v - m.v)
 
-  def *[RD <: Dims](r: Measure[RD]) = Measure[D#Mult[RD]](v * r.v)
-  def /[RD <: Dims](r: Measure[RD]) = Measure[D#Div[RD]](v / r.v)
+  def *[R <: Dims](r: Measure[R]) = Measure[D#Mult[R]](v * r.v)
+  def /[R <: Dims](r: Measure[R]) = Measure[D#Div[R]](v / r.v)
 
   def mult(r: Double) = Measure[D](v * r)
   def div(r: Double) = Measure[D](v / r)
+
+  def >[R <: Dims](r: Measure[R]) = v > r.v
+  def >=[R <: Dims](r: Measure[R]) = v >= r.v
+  def <[R <: Dims](r: Measure[R]) = v < r.v
+  def <=[R <: Dims](r: Measure[R]) = v <= r.v
 
   // def format(implicit formatter: UnitM[D]) = unit.format(this)
 }
