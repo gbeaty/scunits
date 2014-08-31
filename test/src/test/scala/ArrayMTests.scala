12c62db8b566eb2e5fc40bd3170a9fba54ff7784
Grant Beaty <gbeaty@gmail.com>
2014-08-30 21:03:23 -0400

    README and usage examples, now with 'git add .'.

---
+++ test/src/test/scala/ArrayMTests.scala
@@ -0,0 +1,20 @@
+package scunits.test
+
+import scunits._
+import scunits.quantity._
+import scunits.unit.si._
+import scunits.unit.us._
+
+import org.specs2.mutable._
+
+class ArrayMTests extends Specification {
+  val err = 0.0001
+  "Arrays of measurements" should {
+    "Be created correctly" in {
+      true
+    }
+    "Be modified correctly" in {
+      true
+    }
+  }
+}
