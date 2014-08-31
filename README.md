1947845d7dc1a0d2e0b6ecf7699a305b463b48d7
Grant Beaty <gbeaty@gmail.com>
2014-08-30 21:39:52 -0400

    Better README and examples.

--- README.md
+++ README.md
@@ -2,28 +2,28 @@
 
 (or Yet Another Scala Units Library)
 
-scunits is an experimental library for handling measurements, units of measure, quantities and dimensions in scala. Exponents of base quantities are tracked at compile time by type-level recursive list structures similar to shapeless's hlists, but without any run-time instances. To keep things simple, all measurements are stored as Doubles with SI values for their appropriate unit.
+scunits is an experimental library for type-checking dimensional quantities and handling units of measure in scala. Exponents of base quantities are stored at the type-level in recursive list structures similar to shapeless's hlists, but without any run-time instances. To keep things simple, all measurements are stored as Doubles with SI values for their appropriate unit.
 
 ## Features
-- All measurements are Value Classes wrapping Doubles.
-- Users may add new base quantities.
-- Both dimensions and units may be composed.
-- All SI prefixes.
+- All measurements are Value Classes wrapping Doubles
+- Users may add new base quantities
+- Dimensions (`Dims`), units (`UnitM`) and measurements (`Measure`) may be composed by multiplication and division
+- All SI prefixes
 - Array wrappers
 - < 100k jar file size (for now...)
 - No dependencies
 
-Unfortunately the Measure type signatures are rather unreadable.
+Unfortunately dimension type signatures are rather unreadable.
 
 ## Todo
 - BigInt support
 - More units
-- Compile time testing
-- A measure.pow function
-- Logarithmic scale units 
+- Verify compilation times are kept reasonable
+- Dims#Pow, Measure.pow and UnitM.pow functions
+- Logarithmic scale units
 
 ## Using scunits
-See: 
+See: https://github.com/gbeaty/scunits/blob/master/test/src/test/scala/Examples.scala
 
 ## Getting scunits
 scunits is not curretly in a maven repository. You'll have to `git clone https://github.com/gbeaty/scunits.git`, run `sbt publish-local`, then add `"org.scunits" %% "core" % "0.0.1"` to your project dependencies.
--- test/src/test/scala/Examples.scala
+++ test/src/test/scala/Examples.scala
@@ -24,15 +24,27 @@ class Examples extends Specification {
       // All measures are case value classes, and are stored as SI units, so comparisons between measures produce expected results.
       val gal: Measure[Volume] = gallon(1.0)
       val l: Measure[Volume] = litre(1.0)
+      // Measure is the value class which contains the underlying value (Measure.v). Volume is the dimension (Dims).
       gal !=== l
       l ==== cubicMetre(0.001)
 
-      // Use Measure.v to access the underlying double:
+      // Use Measure.v to access the underlying double,
       gal.v must beCloseTo(litre(3.78541).v, err)
+      // This value represents the Measure in its SI unit, e.g. one gallon is so many cubic metres:
+      gal.v ==== 0.00378541
+
+      // Naturally if we do Measure[A] / Measure[A] we get a dimensionless (DNil) result:
+      val dimless: Measure[DNil] = gal / l
+
+      // Type-level Dims composition is easy:
+      implicitly[Volume#Div[Length] =:= Area]
+      implicitly[Acceleration#Mult[Mass] =:= Force]
       
-      // Measure types change as you'd expect:
+      // Dims types change as you'd expect:
       val litreArea: Measure[Area] = l / metre(0.1)
       litreArea ==== squareMetre(0.01)
+      // This does not compile:
+      // litreArea ==== cubicMetre(0.01)
     }
   }
 
@@ -41,19 +53,21 @@ class Examples extends Specification {
       // All SI prefixes are included:
       centi(metre, 10) ==== metre(0.1)
 
-      // You can use prefixes to create new units, e.g.:
+      // You can use prefixes to create new units of measure (UnitM), e.g.:
       val decimetre = deci(metre)
-      decimetre(10) ==== metre(1)
-
-      // Then compose those units:
-      val myLitre = decimetre * decimetre * decimetre
-      myLitre(1).v must beCloseTo(litre(1).v, err)
+      decimetre(10) ==== metre(1)      
     }
   }
 
   "Units" should {
     "Compose" in {
-      // Suppose we need units for fuel consumption:
+      // Any UnitM can be composed by multiplication or division with other UnitMs:
+      val decimetre = deci(metre)
+      val myLitre = decimetre * decimetre * decimetre
+      // A litre is a cubic decimetre:
+      myLitre(1).v must beCloseTo(litre(1).v, err)
+
+      // Suppose we need UnitMs for fuel consumption:
       val kmpL = kilo(metre) / litre
       val mpg = mile / gallon
       mpg(20.0) ==== kmpL(8.502857022092719)
