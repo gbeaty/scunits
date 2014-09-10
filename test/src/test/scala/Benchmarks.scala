7b2db435d3a73daa42df2aecce21a7a1b05d21ff
Grant Beaty <gbeaty@gmail.com>
2014-09-10 03:28:09 -0400

    ArrayM benchmarks.

--- test/src/test/scala/Benchmarks.scala
+++ test/src/test/scala/Benchmarks.scala
@@ -2,6 +2,7 @@ package scunits.test
 
 import scunits._
 import scunits.quantity._
+import scunits.us._
 
 import org.specs2.mutable._
 
@@ -26,67 +27,121 @@ case class ValueClass(v: Double) extends AnyVal {
   def ×(r: Double) = ValueClass(v * r)
 }
 
+case class BenchResult[A](name: String, result: A, time: Long)
+case class BenchResults(name: String, results: Seq[BenchResult[_]])
+
 class Benchmarks extends Specification {
-  sequential  
+  sequential
+
+  def time[A](f: () => A)(name: String) = {
+      val start = System.nanoTime
+      val res = f()
+      val time = (System.nanoTime - start)
+      BenchResult(name, res, time)
+    }
 
   def BenchArithmetic(cycles: Int) = {
     val doubles = Vector.fill(cycles)(util.Random.nextDouble)
     val measures = doubles.map(Measure[Acceleration](_))
     val boxes = doubles.map(Boxed(_))
-    val valueClasses = doubles.map(ValueClass(_))
-
-    def time[A](f: () => A) = {
-      val start = System.nanoTime
-      val res = f()
-      val time = (System.nanoTime - start)
-      (res, time)
-    }
+    val valueClasses = doubles.map(ValueClass(_))    
 
     doubles.foldLeft(0.0)(_+_)
     measures.foldLeft(Measure[Acceleration](0.0))(_+_)
     boxes.foldLeft(Boxed(0.0))(_+_)
     valueClasses.foldLeft(ValueClass(0.0))(_+_)
 
-    val (dSum, dTime) = time(() => doubles.foldLeft(0.0) { (res,el) =>
+    val dRes = time(() => doubles.foldLeft(0.0) { (res,el) =>
       res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5
-    })
-    val (mSum, mTime) = time(() => measures.foldLeft(Measure[Acceleration](0.0)) { (res,el) =>
+    })("Double")
+    val mRes = time(() => measures.foldLeft(Measure[Acceleration](0.0)) { (res,el) =>
       res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5
-    })
-    val (bSum, bTime) = time(() => boxes.foldLeft(Boxed(0.0)) { (res,el) =>
+    }.v)("Measure")
+    val bRes = time(() => boxes.foldLeft(Boxed(0.0)) { (res,el) =>
       res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5 + res / 2.0 + el * 4.0 - (el + res) * 0.5
-    })
-    val (vSum, vTime) = time(() => valueClasses.foldLeft(ValueClass(0.0)) { (res,el) =>
+    }.v)("Boxed")
+    val vRes = time(() => valueClasses.foldLeft(ValueClass(0.0)) { (res,el) =>
       res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5 + res ÷ 2.0 + el × 4.0 - (el + res) × 0.5
-    })
+    }.v)("ValueClass")
 
-    def printTime(n: String, t: Long) = println(n + ": " + (t / 1000000) + " ms")
+    Seq(dRes, mRes, bRes, vRes)
+  }
 
-    println("Results, doubles, measures, boxes, valueClasses:")
-    println(dSum)
-    println(mSum)
-    println(bSum)
-    println(vSum)
+  def BenchArray(cycles: Int) = {
+    val double = Vector.fill(cycles)(util.Random.nextDouble).toArray
+    val measureA = ArrayM[Length](double)
+    val measure = double.map(Measure[Length](_))
 
-    println()
+    import scala.collection.mutable._
 
-    println("Times:")
-    printTime("Doubles",dTime)
-    printTime("Measures",mTime)
-    printTime("Boxes",bTime)
-    printTime("ValueClasses",vTime)
+    def iterate[A](a: ArrayLike[A,_], sum: (A,A) => A) = {
+      var i = 0;
+      while(i < a.length) {
 
-    (dTime, mTime, bTime, vTime)
+        var ii = i
+        while(ii < a.length) {
+          a(i) = sum(a(i), a(ii))
+          ii += 1
+        }
+
+        i += 1
+      }
+    }
+
+    val mop = (l: Measure[Length], r: Measure[Length]) => l + r
+
+    Seq(
+      time(() => iterate(double, (l: Double, r: Double) => l + r))("Array[Double]"),
+      time(() => iterate(measureA, mop))("ArrayM[Measure[Length]]"),
+      time(() => iterate(measure, mop))("Array[Measure[Length]]")
+    )
+  }
+
+  val preJitRes = BenchArithmetic(10000)
+  val jitRes = BenchArithmetic(500000)
+  val preJitArray = BenchArray(300)
+  val jitArray = BenchArray(4000)
+
+  def printAll(brs: (String,Seq[BenchResult[_]])*) {    
+    brs.foreach { brn =>
+      val (n,br) = brn
+      println()
+      println("-- " + n)
+      br.foreach { r => println(r.name + ": " + r.time / 1000000 + " ms") }
+    }
+  }
+
+  printAll(
+    "Pre-JIT Arithmetic"->preJitRes,
+    "JITed Arithmetic"->jitRes,
+    "Pre-JIT Array"->preJitArray,
+    "JITed Array"->jitArray
+  )
+  println()
+
+  "All tests" should {
+    "Produce the same results as Doubles" in {
+      Seq(preJitRes, jitRes).map { brs =>
+        brs.map(_.result).toSet.size ==== 1
+      }
+    }
   }
 
   "Measures" should {
     "Out-perform boxed values before JIT has warmed up" in {
-      val (dTime, mTime, bTime, vTime) = BenchArithmetic(10000)
-      mTime must be_<(bTime)
+      preJitRes(1).time must be_<(preJitRes(2).time)
     }
     "Perform similarly to Doubles once it has warmed up" in {
-      val (dTime, mTime, bTime, vTime) = BenchArithmetic(500000)
-      mTime must be_<(dTime + dTime/10)
+      jitRes(1).time must be_<((jitRes(0).time * 1.1).toLong)
+    }
+  }
+
+  "ArrayM[_]s" should {
+    "Out-perform Array[Measure[_]]" in {
+      jitArray(1).time must be_<(jitArray(2).time)
+    }
+    "Perform like Array[Double]" in {
+      jitArray(1).time must be_<((jitArray(0).time * 1.1).toLong)
     }
   }
 }
