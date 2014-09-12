9373f74f88770caa45c39c3136e1db603843cafc
Grant Beaty <gbeaty@gmail.com>
2014-09-12 02:41:12 -0400

    Renaming the operator implicits.

--- core/src/main/scala/Scunits.scala
+++ core/src/main/scala/Scunits.scala
@@ -12,14 +12,16 @@ object Scunits extends LowPriorityImplicits {
     def compare(l: Measure[D], r: Measure[D]) = if(l < r) -1 else if(l > r) 1 else 0
   }
   
-  implicit def multDNil[L <: Dims](l: Measure[L], r: Measure[DNil], op: Mult.type) = Measure[L](l.v * r.v)
-  implicit def multDenomR[L <: Dims,R <: Dims](l: Measure[L], r: Measure[R#Div[L]], op: Mult.type) = Measure[R](l.v * r.v)
-  implicit def multDenomL[L <: Dims,R <: Dims](l: Measure[L#Div[R]], r: Measure[R], op: Mult.type) = Measure[L](l.v * r.v)
+  implicit def mult_L_DNil[L <: Dims](l: Measure[L], r: Measure[DNil], op: Mult.type) = Measure[L](l.v * r.v)
+  implicit def mult_DNil_L[L <: Dims](l: Measure[DNil], r: Measure[L], op: Mult.type) = Measure[L](l.v * r.v)
+  implicit def mult_L_RdivL[L <: Dims,R <: Dims](l: Measure[L], r: Measure[R#Div[L]], op: Mult.type) = Measure[R](l.v * r.v)
+  implicit def mult_LdivR_R[L <: Dims,R <: Dims](l: Measure[L#Div[R]], r: Measure[R], op: Mult.type) = Measure[L](l.v * r.v)
 
-  implicit def divDNil[L <: Dims,R <: Dims](l: Measure[L], r: Measure[DNil], op: Div.type) = Measure[L](l.v / r.v)
-  implicit def divDenom[L <: Dims,R <: Dims](l: Measure[L], r: Measure[L#Div[R]], op: Div.type) = Measure[R](l.v / r.v)
-  implicit def divNum[L <: Dims,R <: Dims](l: Measure[R#Div[L]], r: Measure[R], op: Div.type) = Measure[L#Neg](l.v / r.v)
-  implicit def divSelf[L <: Dims](l: Measure[L], r: Measure[L], op: Div.type) = Measure[DNil](l.v / r.v)  
+  implicit def div_L_DNil[L <: Dims](l: Measure[L], r: Measure[DNil], op: Div.type) = Measure[L](l.v / r.v)
+  implicit def div_DNil_R[R <: Dims](l: Measure[DNil], r: Measure[R], op: Div.type) = Measure[R#Neg](l.v / r.v)
+  implicit def div_L_LdivR[L <: Dims,R <: Dims](l: Measure[L], r: Measure[L#Div[R]], op: Div.type) = Measure[R](l.v / r.v)
+  implicit def div_RdivL_R[L <: Dims,R <: Dims](l: Measure[R#Div[L]], r: Measure[R], op: Div.type) = Measure[L#Neg](l.v / r.v)
+  implicit def div_L_L[L <: Dims](l: Measure[L], r: Measure[L], op: Div.type) = Measure[DNil](l.v / r.v)  
 
   implicit def invert[F <: Dims](f: Measure[F]) = Measure[F#Neg](1.0 / f.v)
   
