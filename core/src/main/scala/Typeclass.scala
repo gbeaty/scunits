88c467bba23d5a384cf2ae1d0318aeb752c15dc6
Grant Beaty <gbeaty@gmail.com>
2014-09-13 04:40:14 -0400

    Some progress.

--- core/src/main/scala/Typeclass.scala
+++ core/src/main/scala/Typeclass.scala
@@ -4,44 +4,47 @@ import scunits.BaseQuantityLike
 import scunits.integer._
 import scunits.integer.Ops._
 
-trait Dim {
-  type Quant <: BaseQuantity
-  type Exp <: Integer
-  type Mult[R <: DimOf[Quant]] = Quant ^ (Exp + R#Exp)
+object DListOps {
+  type ^[L <: DList,R <: Integer] = L#Pow[R]
 }
-trait DimOf[Q <: BaseQuantity] extends Dim {
-  type Quant = Q
-}
-trait ^[L <: BaseQuantity, R <: Integer] extends Dim {
-  type Quant = L
-  type Exp = R
-}
-
+import DListOps._
 trait DList {
   type Neg <: DList
+  type Pow[R <: Integer] <: DList
+  type Mult[R <: Integer] <: DList
+  type Append[R <: DList] <: DList
 }
 trait DNil extends DList {
   type Neg = DNil
+  type Pow[R <: Integer] = DNil
+  type Mult[R <: Integer] = DNil
+  type Append[R <: DList] = R
 }
-trait DNel extends DList with Dim {
+trait DNel extends DList {
   type Tail <: DList
   type Quant <: BaseQuantity
-  type Exp <: Integer
-  type Neg = (Quant ^ Exp#Neg) * Tail#Neg
+  type Exp <: Integer  
 }
-trait DNelOf[Q <: BaseQuantity] extends DNel with DimOf[Q]
-trait *[L <: Dim, R <: DList] extends DNel {
-  type Tail = R
-  type Quant = L#Quant
-  type Exp = L#Exp
+trait DNelOf[Q <: BaseQuantity] extends DNel {
+  type Quant = Q
+}
+trait DNelTo[E <: Integer] extends DNel {
+  type Exp = E
+}
+trait DNelOfTo[Q <: BaseQuantity, E <: Integer] extends DNelOf[Q] with DNelTo[E]
+trait DNelConst[Q <: BaseQuantity, E <: Integer, T <: DList] extends DNelOfTo[Q,E] {
+  type Tail = T
+
+  type Neg = DNelConst[Quant,Exp#Neg,Tail#Neg]
+  type Pow[R <: Integer] = DNelConst[Quant,Exp#Mult[R],Tail#Pow[R]]
+  type Mult[R <: Integer] = DNelConst[Quant,Exp#Add[R],Tail]
+  type Append[R <: DList] = DNelConst[Quant,Exp,Tail#Append[R]]
 }
 
-trait BaseQuantity extends DNel {
+trait BaseQuantity {
   type Quant <: BaseQuantity
-  type Exp = _1
-  type Tail = DNil
+  type Dim = DNelConst[this.type,_1,DNil]
 }
-trait BaseQuantityOf[Q <: BaseQuantityOf[Q]] extends DNelOf[Q] with BaseQuantity
 
 trait Simplifier[I <: DList] {
   type Out <: DList
@@ -50,24 +53,24 @@ trait SimplifierConst[I <: DList, O <: DList] extends Simplifier[I] {
   type Out = O
 }
 
-class Multer[L <: DList, R <: DList] {
-  type Out <: DList
+class Multer  [L <: DList, R <: DList]                                             { type Out <: DList }
+class MulterOf[L <: DList, R <: DList, RH <: DNel, O <: DList] extends Multer[L,R] { type Out = O }
+trait MulterCombine {
+  // Combine L and R, increment L, and reset R to its head (RH).
+  // implicit def combine[L <: DNel, R <: DNelOf[L#Quant], RH <: DNel](implicit m: Multer[L#Tail,RH]) =
+    // new MulterOf[L,R,RH,m.Out#Append[L#Mult[R#Exp]]]
 }
-class MulterOf[L <: DList, R <: DList, O <: DList] extends Multer[L,R] {
-  type Out = O
-}
-trait MulterSkip {
-  // implicit def skip[L <: DNel, R <: Dim](implicit m: Multer[L#Tail,R])
-}
-trait MulterCancel {
-  implicit def cancel[L <: DNel] = new MulterOf[L,L#Neg,DNil]
-}
-object Multer extends MulterSkip with MulterCancel {
-  implicit def combine[L <: DNel, R <: DNelOf[L#Quant]] = new MulterOf[L,R,L#Mult[R] * R#Tail]
 
-  implicit def dnilRight[L <: DList] = new MulterOf[L,DNil,L]
-  implicit def dnilLeft[R <: DList] = new MulterOf[DNil,R,R]
+trait MulterSkip extends MulterCombine {
+  implicit def zero[L <: DNel, R <: DNelOfTo[L#Quant,L#Exp#Neg], RH <: DNel](implicit m: Multer[L#Tail,RH]) =
+    new MulterOf[L,R,RH,m.Out]
 }
+trait MulterSolve extends MulterSkip {
+  implicit def leftNil[R <: DList] = new Multer[DNil,R] { type Out = R }
+  implicit def rightNil[L <: DList] = new Multer[L,DNil] { type Out = L }
+  implicit val nilNil = new Multer[DNil,DNil] { type Out = DNil }
+}
+object Multer extends MulterSolve
 
 case class Measure[D <: DList](v: Double) extends AnyVal {
   def ===(r: Measure[D]) = v == r.v
@@ -75,16 +78,34 @@ case class Measure[D <: DList](v: Double) extends AnyVal {
   def /[R <: DList](r: Measure[R])(implicit m: Multer[D,R#Neg]) = Measure[m.Out](v / r.v)
 }
 
-trait Length extends BaseQuantityOf[Length]
-trait Mass extends BaseQuantityOf[Mass]
-trait Time extends BaseQuantityOf[Time]
+object Length extends BaseQuantity
+object Mass extends BaseQuantity
+object Time extends BaseQuantity
 
 object Test {
   import Multer._
   def one[D <: DList] = Measure[D](1)
 
+  type Length = Length.Dim
+  type Mass = Mass.Dim
+  type Time = Time.Dim
+
+  one[DNil] * one[DNil]: Measure[DNil]
   one[Length] * one[DNil]: Measure[Length]
   one[DNil] * one[Length]: Measure[Length]
-  one[Length] * one[Length]: Measure[Length ^ _2 * DNil]
-  // one[Length].*(one[(Length ^ _1#Neg) * DNil])(cancel[Length])
+  one[Length]./(one[Length])(zero[Length,Length#Neg,Length#Neg]): Measure[DNil]
+
+  // MulterOf[DNelConst[Length.type,_1,DNil],DNelConst[Length.type,_1,DNil],DNelConst[Length.type,_1,DNil],DNelConst[Length.type,_1,DNelConst[Length.type,_2,DNil]]]
+  // Multer[Test.Length,Test.Length#Neg]
+  // Multer[DNelConst[Length.type,_1,DNil],DNelConst[Length.type,_1#Neg,DNil]]
+  /*MulterOf[
+    DNelConst[Length.type,_1,DNil],
+    DNelConst[Length.type, _1#Neg, DNil],
+    DNil
+  ]
+  // Req:
+  Multer[
+    DNelConst[Length.type,_1,DNil],
+    DNelConst[Length.type,_1#Neg,DNil]
+  ]*/
 }
