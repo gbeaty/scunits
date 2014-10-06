ca289f73d6f975b7630f6427d35d7a96a83861e7
Grant Beaty <gbeaty@gmail.com>
2014-10-06 13:36:21 -0400

    Getting cyclic reference errors.

--- core/src/main/scala/Types/Ordering.scala
+++ core/src/main/scala/Types/Ordering.scala
@@ -28,9 +28,9 @@ trait QuantOrder {
   // protected type comp[R <: QuantOrder] <: Comp
   // protected type compNel[L <: OrderedQuant] <: Comp  
 
-  // type compare[L >: all <: QuantOrder, R >: all <: QuantOrder] = L#comp[R]
-  // protected type comp[R <: QuantOrder] <: Comp
-  // protected type compNel[L <: OrderedQuant] <: Comp
+  type compare[L >: all <: QuantOrder, R >: all <: QuantOrder] = L#comp[R]
+  protected type comp[R <: QuantOrder] <: Comp
+  protected type compNel[L <: OrderedQuant] <: Comp
 }
 trait OrderedQuant extends QuantOrder {
   type head <: Quant
@@ -39,32 +39,34 @@ trait OrderedQuant extends QuantOrder {
 }
 trait LessThan[T <: QuantOrder] extends OrderedQuant {
   type tail = T
-  // type self = LessThan[T]
+  type self = LessThan[T]
   
   type all = this.type with tail#all
-  // protected type comp[R <: QuantOrder] = R#compNel[self]
-  // protected type compNel[L <: OrderedQuant] = L#tail#comp[tail]
+  protected type comp[R <: QuantOrder] = R#compNel[self]
+  protected type compNel[L <: OrderedQuant] = L#tail#comp[tail]
 }
 trait Inf extends QuantOrder {
   type all = Inf
   type empty = True
 
-  // protected type comp[R <: QuantOrder] = R#empty#branch[Comp,Equal,Lesser]
-  // protected type compNel[L <: OrderedQuant] = Greater
+  protected type comp[R <: QuantOrder] = R#empty#branch[Comp,Equal,Lesser]
+  protected type compNel[L <: OrderedQuant] = Greater
 }
 
 object Test {
-  trait C extends LessThan[Inf]
-  trait B extends LessThan[C]
-  trait A extends LessThan[B]
-  type ABC = A
+  object C extends LessThan[Inf]
+  object B extends LessThan[C.type]
+  object A extends LessThan[B.type]
+  type C = C.type
+  type B = B.type
+  type A = A.type
 
-  trait Z extends LessThan[Inf]
-  trait Y extends LessThan[Z]
-  trait X extends LessThan[Y]
-  type XYZ = X
+  object Z extends LessThan[Inf]
+  object Y extends LessThan[Z.type]
+  object X extends LessThan[Y.type]
+  type Z = Z.type
+  type Y = Y.type
+  type X = X.type
 
-  type has[Q >: ABC#all] = True
-  // type h1 = ABC#has[A]
-  type h2 = has[A]
+  type c1 = A#compare[A,B]
 }
