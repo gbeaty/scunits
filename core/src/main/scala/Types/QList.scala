3d22ec9d9474f81be9f98e6fcedb51cc17d1d7ac
Grant Beaty <gbeaty@gmail.com>
2014-10-22 02:40:06 -0400

    Compile performance fixed.

--- core/src/main/scala/Types/QList.scala
+++ core/src/main/scala/Types/QList.scala
@@ -1,7 +1,6 @@
 package scunits.types
 
 trait QList {
-  type self <: QList
   type set <: BaseQuantity
   type dims <: Dims   
 
@@ -10,10 +9,9 @@ trait QList {
   type zeros <: dims
   type append[As <: QList] <: QList
 
-  type op[L <: Dims, R <: Dims, O[_ <: Integer, _ <: Integer] <: Integer] = ({
-    type zeros = self#zeros
-    type res = self#bugOp[zeros with L, zeros with R, O]
-  })#res
+  type op[L <: Dims, R <: Dims, O[_ <: Integer, _ <: Integer] <: Integer] =
+    bugOp[zeros with L, zeros with R, O]
+
   type mult[L <: Dims, R <: Dims] = op[L,R,+]
   type div[L <: Dims, R <: Dims] = op[L,R,-]
 
@@ -32,7 +30,6 @@ class ::[L <: BaseQuantity, R <: QList] extends QListOf[L#of with R#dims] {
   type head = L
   type tail = R
   type dims = L#of with R#dims
-  type self = head :: tail
   type set = head with tail#set
   type zeros = head#setDim[tail#zeros, _0]
   type append[As <: QList] = head :: tail#append[As]
@@ -43,7 +40,6 @@ class ::[L <: BaseQuantity, R <: QList] extends QListOf[L#of with R#dims] {
     head#setNonZero[tail#bugOp[L,R,O], O[head#get[L], head#get[R]]]
 }
 trait QNil extends QList {
-  type self = QNil
   type set = BaseQuantity
   type dims = Dims
   type zeros = Dims
